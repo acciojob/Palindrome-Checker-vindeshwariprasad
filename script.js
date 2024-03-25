745615d271eeb39b5113c1485bcb39dfc8230ae7
Vindeshwari Prasad <vinayguptasia53@gmail.com>
2024-03-25 19:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 // complete the given function
 
 function palindrome(str){
+	let a = ""
+	for (let s of str){
+		if (s!===" "){
+			a+=s.toLowerCase();
+		}
+		
+	}
+	let n = a.length;
+	let i = 0;
+	let j = n-1;
+	while (j>i){
+		if (a[i]!===a[j]){
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
 
 }
 module.exports = palindrome
